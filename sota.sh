6b190e903efb7cceeac0ca897300da4a0e63a540
Aananth Chellapa Natarajan <c.n.aananth@gmail.com>
2015-10-01 11:31:12 +0530

    Corrected paths from relative to absolute in sota.sh

--- sota.sh
+++ sota.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
-if [ -f base-version/update_info.json ]; then
-	rm -f base-version/update_info.json
+SOTABASE=/home/root
+
+if [ -f ${SOTABASE}/base-version/update_info.json ]; then
+	rm -f ${SOTABASE}/base-version/update_info.json
 fi
 
 sotaclient -i /etc/client_info.json -s 122.165.96.181
 
-if [ -f base-version/update_info.json ]; then
-	sotaupdater -i base-version/update_info.json -o /etc/client_info.json -f
+if [ -f ${SOTABASE}/base-version/update_info.json ]; then
+	sotaupdater -i ${SOTABASE}/base-version/update_info.json -o /etc/client_info.json -f
 fi
 
